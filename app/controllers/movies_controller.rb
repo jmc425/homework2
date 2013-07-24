383a4a0c25b3310ece4f9f5bd73fed67ec25812e
jmc425 <jmc425@server.fake>
2013-07-24 21:21:18 +1200

    Got header links working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,13 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if params[:sort] == "title"
+      @movies = Movie.order("title ASC").all
+    elsif params[:sort] == "release_date"
+      @movies = Movie.order("release_date ASC").all
+    else
+      @movies = Movie.all
+    end
   end
 
   def new
